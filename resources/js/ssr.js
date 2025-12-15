716dfbb26e4305fdeccaf46c46901f8b0bec6fea
Amaranese <32201849+Amaranese@users.noreply.github.com>
2025-12-15 23:04:22 +0100

    Update ssr.js

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,22 +1,18 @@
+import { createSSRApp, h } from 'vue';
+import { renderToString } from '@vue/server-renderer';
 import { createInertiaApp } from '@inertiajs/vue3';
 import createServer from '@inertiajs/vue3/server';
-import { renderToString } from '@vue/server-renderer';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { createSSRApp, h } from 'vue';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
+import { ZiggyVue } from '../../vendor/tightenco/ziggy/dist/vue.m';
 
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+const appName = 'Laravel';
 
 createServer((page) =>
     createInertiaApp({
         page,
         render: renderToString,
         title: (title) => `${title} - ${appName}`,
-        resolve: (name) =>
-            resolvePageComponent(
-                `./Pages/${name}.vue`,
-                import.meta.glob('./Pages/**/*.vue'),
-            ),
+        resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
         setup({ App, props, plugin }) {
             return createSSRApp({ render: () => h(App, props) })
                 .use(plugin)
@@ -25,5 +21,5 @@ createServer((page) =>
                     location: new URL(page.props.ziggy.location),
                 });
         },
-    }),
+    })
 );
